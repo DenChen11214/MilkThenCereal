d0c5eebb84c25734ab0d34a1085ff3298f33d5e8
denchen11214 <dchen22@stuy.edu>
2019-04-02 20:34:12 -0400

    made a grouped bar graph of sales vs genres with different bars for each region

--- barGraph/demo.js
+++ barGraph/demo.js
@@ -2,10 +2,10 @@ var margin = {top: 40, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var formatPercent = d3.format(".0%");
-
 var x = d3.scale.ordinal()
     .rangeRoundBands([30, width], .1);
+
+var x1 = d3.scale.ordinal();
 
 var y = d3.scale.linear()
     .range([height, 0]);
@@ -25,6 +25,9 @@ var tip = d3.tip()
     return "<strong>Global Sales:</strong> <span style='color:red'>" + d.values.Global_Sales + "</span>";
   })
 
+var color = d3.scale.ordinal()
+    .range(["#ca0020","#f4a582","#d5d5d5","#92c5de"]);
+
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom + 200)
@@ -33,13 +36,30 @@ var svg = d3.select("body").append("svg")
 
 svg.call(tip);
 
-d3.csv("VideoGameSales.csv", type, function(error, data) {
+d3.csv("VideoGameSales.csv", function(error, data) {
   var nested_data = d3.nest()
     .key(function(d) { return d.Genre; })
-    .rollup(function(leaves) { return {"Global_Sales": d3.sum(leaves, function(d) {return parseFloat(d.Global_Sales);})} })
+    .rollup(function(leaves) {
+      return {"NA_Sales": d3.sum(leaves, function(d) {return parseFloat(d.NA_Sales);}),
+              "EU_Sales": d3.sum(leaves, function(d) {return parseFloat(d.EU_Sales);}),
+              "JP_Sales": d3.sum(leaves, function(d) {return parseFloat(d.JP_Sales);}),
+              "Other_Sales": d3.sum(leaves, function(d) {return parseFloat(d.Other_Sales);})}})
     .entries(data);
-  x.domain(nested_data.map(function(d) { return d.key; }));
-  y.domain([0, d3.max(nested_data, function(d) { return d.values.Global_Sales; })]);
+  var region = nested_data.map(function(d,i){
+    area = []
+    for(value in d.values){
+      area.push(value)
+    }
+    return area[i]}).splice(0,4)
+  var genre = nested_data.map(function(d){return d.key});
+  x.domain(genre);
+  x1.domain(region).rangeRoundBands([0,x.rangeBand()]);
+  y.domain([0, d3.max(nested_data, function(d) {
+    sales = []
+    for(value in d.values){
+      sales.push(d.values[value])
+    }
+    return Math.max.apply(Math, sales)})]);
 
   svg.append("text")
         .attr("x", (width / 2))
@@ -47,7 +67,7 @@ d3.csv("VideoGameSales.csv", type, function(error, data) {
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "underline")
-        .text("Global Sales vs Genre");
+        .text("Sales vs Genre");
   svg.append("text")
       .attr("class", "x label")
       .attr("x", width / 2)
@@ -62,7 +82,7 @@ d3.csv("VideoGameSales.csv", type, function(error, data) {
       .attr("y", -15)
       .attr("font-size","16px")
       .style("text-anchor", "middle")
-      .text("Global Sales");
+      .text("Sales");
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -71,20 +91,73 @@ d3.csv("VideoGameSales.csv", type, function(error, data) {
       .attr("class", "y axis")
       .attr("transform", "translate(30,0)")
       .call(yAxis)
-  svg.selectAll(".bar")
+  var slice = svg.selectAll(".slice")
       .data(nested_data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.key); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) {return y(d.values.Global_Sales); })
-      .attr("height", function(d) { return height - y(d.values.Global_Sales); })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
+      .enter().append("g")
+      .attr("class", "g")
+      .attr("transform",function(d) { return "translate(" + x(d.key) + ",0)"; });
+  slice.selectAll("rect")
+      .data(function(d) {
+        sales = []
+        for(value in d.values){
+          var dict = {};
+          dict[value] = d.values[value]
+          sales.push(dict)
+        }
+        return sales; })
+  .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d,i) { return x1(region[i]); })
+      .style("fill", function(d,i) { return color(region[i]) })
+      .attr("y", function(d) { return y(0); })
+      .attr("height", function(d) { return height - y(0); })
+      .on("mouseover", function(d,i) {
+          d3.select(this).style("fill", d3.rgb(color(region[i])).darker(2));
+      })
+      .on("mouseout", function(d,i) {
+          d3.select(this).style("fill", color(region[i]));
+      });
+  slice.selectAll("rect")
+      .transition()
+      .delay(function (d) {return Math.random()*1000;})
+      .duration(1000)
+      .attr("y", function(d,i) {
+      for(value in d){
+        sales = d[value]
+      }
+      return y(sales)})
+      .attr("height", function(d,i) {
+      for(value in d){
+        sales = d[value]
+      }
+      return height - y(sales)});
 
+  sales = []
+  sample = nested_data[0]["values"]
+  for(value in sample){
+    var dict = {};
+    dict[value] = sample[value]
+    sales.push(dict)
+  }
+  console.log(sales)
+  var legend = svg.selectAll(".legend")
+      .data(sales.map(function(d,i) { return region[i]; }).reverse())
+  .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d,i) { return "translate(0," + i * 20 + ")"; })
+      .style("opacity","0");
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", function(d) { return color(d); });
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) {return d; });
+  legend.transition().delay(function(d,i){ return 1300 + 100 * i; }).style("opacity","1");
 });
-
-function type(d) {
-  d.Global_Sales = +d.Global_Sales;
-  return d;
-}
