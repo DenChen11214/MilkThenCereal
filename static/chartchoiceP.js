edfc9851d43618069fcd7aefd2907a707b924009
denchen11214 <dchen22@stuy.edu>
2019-04-08 18:30:40 -0400

    added description for bar graph

--- static/chartchoiceP.js
+++ static/chartchoiceP.js
@@ -1,13 +1,13 @@
 var p = document.getElementById("chartchoiceP");
-
-var s1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod mauris ut metus rutrum porta. Quisque sed elit at diam porttitor tempor. Vivamus porttitor, nunc id faucibus tristique, risus tellus finibus massa, ut molestie urna augue sit amet risus. Sed non ullamcorper enim, et semper libero. Integer id imperdiet dui.";
-var s2 = "Doggo ipsum lotsa pats you are doin me a concern yapper very taste wow long doggo fat boi waggy wags, sub woofer lotsa pats fluffer tungg very hand that feed shibe. Porgo smol doge sub woofer yapper sub woofer, most angery pupper I have ever seen smol long bois.";
+var b = document.getElementById("chartchoiceB");
+var s1 = "This is an interactive pie chart. You can choose the divide sections of the pie chart into different categories based on what you select on the drop down below. Hover over sections of the pie chart to see the number and percentage of games occupied by it. If you click on the section, a list of games will appear. You can click on at most two sections to compare the games contained side by side. You can only select at most 2 sections at a time! To reset the pie chart just click the center.";
+var s2 = "This is an interactive bar graph. You can choose a category in the dropdown, and the graph will change to create a bar graph of Sales vs The Category You Chose. There are four different colored bars representing different regions for each item in the category you chose. Hover over a bar to see the number of sales for that item in that specific region."
 
 function sleep(ms){
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function update(s){
+async function update(s,p){
 	p.innerHTML = "";
 	var l = s.length;
 	var ms = 2000 / l;
@@ -29,7 +29,11 @@ function showBar(){
   document.getElementById('bar').style.display = 'block';
 }
 var pc = document.getElementById("piechoice");
-pc.addEventListener("click", function(){update(s1,showPie())});
+pc.addEventListener("click", function(){
+	update(s1,p)
+	showPie()});
 
 var bc = document.getElementById("barchoice");
-bc.addEventListener("click", function(){update(s2), showBar()});
+bc.addEventListener("click", function(){
+	update(s2,b)
+	showBar()});
